66137f04754d6bafd59d464bee24757885a3575a
Adam Mitchell <adamryanmitchell15@gmail.com>
2021-04-25 17:37:37 -0500

    - Adding in a couple of components we'll be using for tdd practice and a fun project we'll be doing: conway's game of life

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { GameOfLifeComponent } from './game-of-life/game-of-life.component';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'game-of-life', component: GameOfLifeComponent },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameOfLifeComponent } from './game-of-life/game-of-life.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameOfLifeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
